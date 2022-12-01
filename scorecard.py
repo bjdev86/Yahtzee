3ad4b89e94b3f8e40d4a2de36c3724f532c6d12a
Ben Miller <bjmiller86@gmail.com>
2022-11-30 22:32:36 -0800

    Scorecard object used to maintain a player's scorable category scores for all rounds of a game. This scorecard object will be comsumed by the game module to determine a player's score at the end of the game.

---
+++ scorecard.py
@@ -0,0 +1,14 @@
+# Class (Struct) to define the all the categories of the score card. All a 
+# plyer's scores for a whole game (3 rounds) are store in this object 
+
+class ScoreCard: 
+    def __init__():
+        self.round = [
+            dict.fromkeys(scorableCombos, initialComboVals),
+            dict.fromkeys(scorableCombos, initialComboVals),
+            dict.fromkeys(scorableCombos, initialComboVals)
+        ]
+
+# Dictionary to define the categaries to score in for each round 
+scorableCombos = ("one", "two", "three", "four", "five", "six", "pair", )
+initialComboVals = ""
